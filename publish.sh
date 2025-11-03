10e3b4dff55d69e9701120859d8a9546e3682ba3
zabbits <git@zabbits.com>
2025-11-03 17:37:43 +0800

    chore: add obsidian-export ignore file

--- publish.sh
+++ publish.sh
@@ -1,4 +1,4 @@
 rm -rf dist .astro node_modules/.astro && \
-    mkdir -p /tmp/blog_build && obsidian-export --publish-only ~/vaults/nichijou /tmp/blog_build && \
+    mkdir -p /tmp/blog_build && obsidian-export --publish-only --ignore-file ~/vaults/nichijou/.export-ignore ~/vaults/nichijou /tmp/blog_build && \
     CONTENT_PATH_BASE=/tmp/blog_build pnpm build && \
     rm -rf /tmp/blog_build
