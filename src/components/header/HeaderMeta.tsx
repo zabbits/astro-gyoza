4aba3c8d81b438e1290b091e2d4e122f87421813
zabbits <git@zabbits.com>
2025-11-04 13:20:40 +0800

    chore: page width

--- src/components/header/HeaderMeta.tsx
+++ src/components/header/HeaderMeta.tsx
@@ -10,7 +10,7 @@ export function HeaderMeta() {
     <AnimatePresence>
       {shouldShow && (
         <motion.div
-          className="absolute inset-0 z-1 flex items-center justify-between md:px-10"
+          className="absolute inset-0 z-1 flex items-center justify-between md:px-4"
           initial={{
             opacity: 0,
             y: 20,
