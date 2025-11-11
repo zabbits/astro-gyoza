2659fd90af2453eaded0d475622dd8b8528b04cc
zabbits <git@zabbits.com>
2025-11-11 17:01:27 +0800

    chore: publish.sh read BASE_DIR

--- publish.sh
+++ publish.sh
@@ -1,4 +1,8 @@
+BASE_DIR=$1
+TMP_DIR=/tmp/blog_build
 rm -rf dist .astro node_modules/.astro && \
-    mkdir -p /tmp/blog_build && obsidian-export --publish-only --ignore-file ~/vaults/nichijou/.export-ignore ~/vaults/nichijou /tmp/blog_build && \
-    CONTENT_PATH_BASE=/tmp/blog_build pnpm build && \
-    rm -rf /tmp/blog_build
+    mkdir -p /tmp/blog_build && \
+    obsidian-export --publish-only --ignore-file "${BASE_DIR}/.export-ignore" "${BASE_DIR}" "${TMP_DIR}" && \
+    cp -r "${BASE_DIR}/projects" "${TMP_DIR}" && \
+    CONTENT_PATH_BASE="${TMP_DIR}" pnpm build && \
+    rm -rf "${TMP_DIR}"
